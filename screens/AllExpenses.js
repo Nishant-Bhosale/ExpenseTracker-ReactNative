176b45e660ce1c9680cee638090aad0ab6ef4e6a
Nishant Bhosale <nishantbhosale244@gmail.com>
2022-09-11 10:10:48 +0530

    Added screens and navigation packages

---
+++ screens/AllExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const AllExpenses = () => {
+	return <Text>AllExpenses Screen</Text>;
+};
+
+export default AllExpenses;
---
+++ screens/ManageExpense.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const ManageExpense = () => {
+	return <Text>ManageExpense Screen</Text>;
+};
+
+export default ManageExpense;
